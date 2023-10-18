ff5caea70fe13fa495dc090fbc66995e9f858076
yashYJ621 <yashbiz621@gmail.com>
2023-10-18 11:41:50 -0400

    style: apply black formatting
    change in string formatting, update sql query strings, use double quotes for strings

--- dashboard/apiclient.py
+++ dashboard/apiclient.py
@@ -10,7 +10,7 @@ def get_projects_list() -> Dict:
     The main server holds configuraiton and models - get the list.
     :return:
     """
-    path = FEMINICIDE_API_URL + '/api/story_processor/projects.json'
+    path = FEMINICIDE_API_URL + "/api/story_processor/projects.json"
     return _get_json(path)
 
 
@@ -19,12 +19,13 @@ def get_language_models_list() -> Dict:
     Each project can refer to one or two models - get them all.
     :return:
     """
-    path = FEMINICIDE_API_URL + '/api/story_processor/language_models.json'
+    path = FEMINICIDE_API_URL + "/api/story_processor/language_models.json"
     return _get_json(path)
 
 
 def _get_json(path: str) -> Dict:
     params = dict(apikey=FEMINICIDE_API_KEY)
-    r = requests.get(path, params,
-                     timeout=(3.05*20)*10)  # docs say set it to slightly larger than a multiple of 3, so 10ish minutes
+    r = requests.get(
+        path, params, timeout=(3.05 * 20) * 10
+    )  # docs say set it to slightly larger than a multiple of 3, so 10ish minutes
     return r.json()
--- dashboard/test/test_projects.py
+++ dashboard/test/test_projects.py
@@ -4,14 +4,13 @@ import dashboard.projects as projects
 
 
 class TestProjects(unittest.TestCase):
-
     def test_update_list(self):
         project_list = projects.load_project_list(True)
         assert len(project_list) > 0
         for p in project_list:
-            assert 'id' in p
-            assert 'language_model_id' in p
-            assert 'rss_url' in p
+            assert "id" in p
+            assert "language_model_id" in p
+            assert "rss_url" in p
 
 
 if __name__ == "__main__":
