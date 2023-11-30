4a60d9662c6743fc867a150670b3849b4366a84e
yashYJ621 <yashbiz621@gmail.com>
2023-11-30 13:31:36 -0500

    feat(project-reports): new charts and feature for project level report page

--- pages/Project Reports.py
+++ pages/Project Reports.py
@@ -1,4 +1,6 @@
 # pages/project_Reports.py
+# Create a DataFrame from the list of dictionaries
+import pandas as pd
 import streamlit as st
 
 import dashboard.database.alerts_db as alerts
@@ -7,7 +9,7 @@ import dashboard.projects as projects
 from dashboard import sh_functions as helper
 
 # Projects
-st.title("Projects")
+st.sidebar.title("Projects")
 list_of_projects = projects.load_project_list(
     force_reload=True, download_if_missing=True
 )
@@ -23,22 +25,13 @@ titles = ["Click Here to Get A Project's Report"] + [
 ]
 
 # Display the selectbox with the updated titles
-option = st.selectbox("Select Project by ID", (titles))
+option = st.sidebar.selectbox("Select Project by ID", (titles))
 
 # Get the selected project ID
 if option != "Click Here to Get A Project's Report":
     selected_project_id = int(option.split(" - ")[0])
     selected = [p for p in sorted_list_of_projects if p["id"] == selected_project_id][0]
-    st.markdown(
-        """
-    <style>
-    [data-testid="stMetricValue"] {
-        font-size: 25px;
-    }
-    </style>
-    """,
-        unsafe_allow_html=True,
-    )
+
     # Project Attributes
     st.caption("Project Background")
     st.markdown(f"{selected['id']} - {selected['title']}")
@@ -61,24 +54,19 @@ if option != "Click Here to Get A Project's Report":
     except ZeroDivisionError:
         above_threshold_pct = 100
 
-    st.markdown(
-        """
-    <style>
-    [data-testid="stMetricValue"] {
-        font-size: 25px;
-    }
-    </style>
-    """,
-        unsafe_allow_html=True,
-    )
+
+    # Project Statistics
     st.caption("Project Specific Story Statistics")
+
+    # Display metrics
     col1, col2 = st.columns(2)
-    col1.metric("Average Above Threshold Percentage", f"{above_threshold_pct}%")
+    col1.metric("Average Above Threshold Stories Percentage", f"{above_threshold_pct}%")
     col2.metric("Unposted Above Threshold Stories", unposted_above_story_count)
+
     col3, col4 = st.columns(2)
-    col3.metric("Posted Above Threshold Stories", posted_above_story_count)
+    col3.metric("Above Threshold Stories Posted to Email Alerts Server", posted_above_story_count)
     col4.metric("Below Threshold Stories", below_story_count)
-    st.divider()
+
 
     # Model Scores
     st.subheader("Model Scores")
@@ -89,46 +77,62 @@ if option != "Click Here to Get A Project's Report":
     helper.draw_model_scores(project_id=selected["id"])
     st.divider()
 
-    st.subheader("Above Threshold Stories")
+    st.subheader("Above Threshold Stories by Project")
     # by posted day
-    st.caption("Platform Stories by Posted Day")
+    st.caption("Stories sent to the email alerts server based on the day they were run against the classifiers, "
+           "grouped by the data source they originally came from.")
     helper.draw_graph(processor_db.stories_by_posted_day, selected["id"])
     st.divider()
     # History (by discovery date)
-    st.subheader("History")
-    st.caption("Platform Stories by Published Day")
+    st.subheader("History of the Project")
+    st.caption("Stories discovered on each platform based on the guessed date of publication, grouped by the "
+           "data source they originally came from.")
     helper.draw_graph(processor_db.stories_by_published_day, selected["id"])
-    st.caption("Platform Stories by Discovery Day")
+    st.caption("Stories grouped by Platforms based on Discovery Day")
     helper.draw_graph(processor_db.stories_by_processed_day, selected["id"])
-    st.caption("Platform Stories by Discovery Day")
+    st.caption("Stories based on the date they were run against the classifiers, grouped by whether they were above"
+           "threshold for their associated project or not.")
     helper.story_results_graph(selected["id"])
     st.divider()
 
+      
+    st.subheader("Latest Stories in the Project")
+    st.caption("Recent Above threshold Stories")
+    stories_above = processor_db.recent_stories(selected['id'], True)
+    helper.latest_stories(stories_above)
+
+    st.caption("Recent Below threshold Stories")
+    stories_below = processor_db.recent_stories(selected['id'], False)
+    helper.latest_stories(stories_below)
+    
+    st.divider()
+    
     # Add a section for Email-Alerts database visualizations
     st.title("Email-Alerts Database")
 
-    # Total story count in Email-Alerts
-    total_email_alerts_story_count = alerts.total_story_count()
-    st.subheader("Total Story Count in Email-Alerts Database")
-    st.write(f"Total Stories: {total_email_alerts_story_count}")
+    # Total story count in Email-Alerts for the selected project
+    total_email_alerts_story_count = alerts.total_story_count(project_id=selected_project_id)
 
-    # Top 10 media sources/domains by story volume
-    st.subheader("Top 10 Media Sources/Domains by Story Volume")
-    top_media_sources = alerts.top_media_sources_by_story_volume()
-    st.write(top_media_sources)
+    # Use st.metric instead of st.write to display the count more prominently
+    st.metric(label=f"Total Stories in Email-Alerts for Project {selected_project_id} - {selected['title']}", value=total_email_alerts_story_count)
 
-    # Email-Alerts Story volume by publication date
-    st.subheader("Story Volume by Publication Date (Email-Alerts Database)")
-    email_alerts_publication_chart = alerts.email_alerts_stories_by_date_column(
-        "publish_date"
-    )
-    # Visualize the chart using Altair or other plotting libraries
-    st.write(email_alerts_publication_chart)
+    #top media sources 
+    st.subheader("Top 10 media sources by story count")
+    helper.draw_bar_chart_sources(alerts.top_media_sources_by_story_volume_22, project_id=selected["id"])
+    st.divider()
+        # In your main code
+# ...
 
-    # Email-Alerts Story volume by discovery/creation date
-    st.subheader("Story Volume by Discovery/Creation Date (Email-Alerts Database)")
-    email_alerts_discovery_chart = alerts.email_alerts_stories_by_date_column(
-        "created_at"
-    )
-    # Visualize the chart using Altair or other plotting libraries
-    st.write(email_alerts_discovery_chart)
+    # Story Count by Publication Date
+    st.subheader("Story Count by Publication Date")
+    helper.alerts_draw_graph(alerts.stories_by_publish_date, project_id=selected["id"])
+    
+    st.divider()
+
+    # Story Count by Creation Date
+    st.subheader("Story Count by Creation Date")
+    helper.alerts_draw_graph(alerts.stories_by_creation_date, project_id=selected["id"])
+    
+    st.divider()
+
+
