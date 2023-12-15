5fc68ea36c4ccb39323e2a015a155b0d69b3e5ec
Rahul Bhargava <rahulbot@gmail.com>
2023-12-15 14:13:18 -0500

    version bump for release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 Change Log
 ==========
 
+### v0.1.0
+
+* Add first pass at project pages
+
 ### v0.0.2
 
-First deployment, just monitoring story-processor-db data inefficiently.
+* First deployment, just monitoring story-processor-db data inefficiently.
--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -6,7 +6,7 @@ import sentry_sdk
 from dotenv import load_dotenv
 from sentry_sdk.integrations.tornado import TornadoIntegration
 
-VERSION = "0.0.2"
+VERSION = "0.1.0"
 # SOURCE_GOOGLE_ALERTS = "google-alerts"
 SOURCE_MEDIA_CLOUD = "media-cloud"
 SOURCE_NEWSCATCHER = "newscatcher"
